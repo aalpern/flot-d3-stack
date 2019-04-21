065d0312cbf8f0221c730e234c5939625858276a
Adam Alpern <adam.alpern@gmail.com>
2019-04-21 10:50:00 -0700

    Update for D3 4.0

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # d3 Stacking Plugin for Flot
 
 A plugin for Flot which utilizes
-[d3's stack layout](https://github.com/mbostock/d3/wiki/Stack-Layout)
+[d3's stack layout](https://github.com/d3/d3-shape#stacks)
 to provide [stream graphs](http://www.leebyron.com/else/streamgraph/).
 
 ### Usage
 
-There's a option to enable d3 stacking, and an `offset` parameter to control the stacking layout algorithm. Valid values are `wiggle`, `silhouette`, `expand`, and `zero`:
+There's a option to enable d3 stacking, and an `offset` parameter to
+control the stacking layout algorithm. Valid values are `wiggle`,
+`silhouette`, `expand`, and `diverging`. Any other value (e.g. `none`
+or an empty value) will result in a regular stack:
 
 ```javascript
 series: {
@@ -30,3 +33,13 @@ npm install flot-d3-stack
 ![](example/example.png)
 
 * flot-d3-stack is also in use in [Tessera](https://github.com/urbanairship/tessera).
+
+## Release Notes
+
+
+### 2.0
+
+* Release 2.0 has been re-written to work with the new `d3-stack`
+  module from D3 4.0, and is incompatible with D3 3.0.
+* This version has been tested with Flot 0.8.3. Any changes necessary
+  for newer versions of Flot will be made in an upcoming release.
--- jquery.flot.d3.stack.js
+++ jquery.flot.d3.stack.js
@@ -1,46 +1,106 @@
 (function ($) {
   var options = {
-      series: {
-        stackD3: {
-          show: false
-        }
+    series: {
+      stackD3: {
+        show: false
       }
+    }
   }
 
-  function init(plot) {
-    var stack = d3.layout.stack()
-                  .values(function(series) { return series.data } )
-                  .x(function(d) { return d[0] })
-                  .y(function(d) { return d[1] })
-    var stacked = null
+  function getOffset(name) {
+    switch(name) {
+    case 'wiggle':
+      return d3.stackOffsetWiggle
+    case 'silhouette':
+      return d3.stackOffsetSilhouette
+    case 'diverging':
+      return d3.stackOffsetDiverging
+    case 'expand':
+      return d3.stackOffsetExpand
+    case 'stack':
+    default:
+      return d3.stackOffsetNone
+    }
+  }
 
-    function processRawData(plot, series) {
-      if (!series.stackD3 || (series.stackD3 && !series.stackD3.show))
+  function getOrder(name) {
+    switch(name) {
+    case 'ascending':
+      return d3.stackOrderAscending
+    case 'descending':
+      return d3.stackOrderDescending
+    case 'insideout':
+      return d3.stackOrderInsideOut
+    case 'reverse':
+      return d3.stackOrderReverse
+    default:
+      return d3.stackOrderNone
+    }
+  }
+  
+  function init(plot) {
+    var stacked = null
+    
+    function processRawData(plot) {
+      var options = plot.getOptions()      
+      if (!options.series.stackD3 || (options.series.stackD3 && !options.series.stackD3.show))
         return
+      
       if (!stacked) {
-        stack.offset(series.stackD3.offset || 'wiggle')
-        stacked = stack(plot.getData())
+        var data = plot.getData()
+        
+        // Convert data to d3 4.0's expected tabular input format for
+        // stack(). First grab the x values from the first series,
+        // then iterate through all of them to get the data into
+        // attributes.        
+        var d3data = data[0].data.map((point) => {
+          return { x: point[0] }
+        })
+        var keys = []
+        data.forEach((series) => {
+          keys.push(series.label)
+          series.data.forEach((point, i) => {
+            d3data[i][series.label] = point[1]
+          })
+        })
+
+        // Compute the stacked values        
+        var stack = d3.stack()
+          .keys(keys)
+          .offset(getOffset(options.series.stackD3.offset || 'wiggle'))
+          .order(getOrder(options.series.stackD3.order))
+
+        stacked = stack(d3data)
+        // XXX: This probably doesn't work correctly with any of the
+        // d3-stack ordering options other than 'none'
+        stacked.forEach((s, i) => {
+          data[i].d3stacked = s
+        })
       }
     }
 
-    function streamData(plot, series, datapoints) {
-      if (!series.stackD3 || (series.stackD3 && !series.stackD3.show))
+    function processDatapoints(plot, series, datapoints) {
+      var options = plot.getOptions()      
+      if (!options.series.stackD3 || (options.series.stackD3 && !options.series.stackD3.show))
         return
+
+      console.log("processDatapoints", series, datapoints)
+      
       var newpoints = []
       var percents = []
-      series.data.forEach(function(point) {
+      series.data.forEach(function(point, i) {
         newpoints.push(point[0])
-        newpoints.push(point.y0 + point.y)
-        newpoints.push(point.y0)
+        newpoints.push(series.d3stacked[i][0])
+        newpoints.push(series.d3stacked[i][1])
         percents.push(point.y)
       })
-      if (series.stackD3.offset === 'expand')
+      if (options.series.stackD3.offset === 'expand')
         series.percents = percents
       datapoints.points = newpoints
     }
-
+      
     plot.hooks.processRawData.push(processRawData)
-    plot.hooks.processDatapoints.push(streamData)
+    plot.hooks.processDatapoints.push(processDatapoints)
   }
 
   $.plot.plugins.push({
